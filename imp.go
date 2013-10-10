8370b7d58a707ec35b3d3c1ee01b58ca3a0a08a3
Chucky Ellison <cme@freefour.com>
2013-10-10 00:33:48 -0400

    some changes to imp

--- imp.go
+++ imp.go
@@ -1,12 +1,63 @@
 package main
 import "fmt"
 import "strings"
-import "strconv"
+// import "strconv"
 import "time"
 import "runtime/pprof"
 import "os"
 
 const printDebug = false
+const upto = Int64Label(1000000)
+
+var label_hole KLabel = StringLabel(0)
+var label_semi KLabel = StringLabel(100)
+var label_var KLabel = StringLabel(101)
+var label_skip KLabel = StringLabel(102)
+var label_plus KLabel = StringLabel(103)
+var label_neg KLabel = StringLabel(104)
+var label_true KLabel = StringLabel(105)
+var label_false KLabel = StringLabel(106)
+var label_assign KLabel = StringLabel(107)
+var label_while KLabel = StringLabel(108)
+var label_if KLabel = StringLabel(109)
+var label_not KLabel = StringLabel(110)
+var label_lte KLabel = StringLabel(111)
+var label_n KLabel = StringLabel(1000)
+var label_s KLabel = StringLabel(1001)
+
+var names map[StringLabel]string = map[StringLabel]string{
+	0: "hole",
+	100: ";",
+	101: "var",
+	102: "skip",
+	103: "plus",
+	104: "-",
+	105: "true",
+	106: "false",
+	107: ":=",
+	108: "while",
+	109: "if",
+	110: "not",
+	111: "<=",
+	1000: "n",
+	1001: "s",
+}
+
+var hole *K = &K{label_hole, nil, false, false}
+
+type KLabel interface{
+	String() string
+}
+
+type StringLabel uint32
+type Int64Label int64
+
+func (s StringLabel) String() string {
+	return names[s]
+}
+func (s Int64Label) String() string {
+	return fmt.Sprintf("%d", s)
+}
 
 type K struct {
 	label KLabel
@@ -18,18 +69,21 @@ func (k K) String() string {
 	return k.label.String() + "(" + k.args.String() + ")"
 }
 func (k *K) Copy() *K {
-	var newArgs ListK = nil
-	for _, arg := range k.args {
-		newArgs = append(newArgs, arg.Copy())
+	if len(k.args) == 0 {
+		return k
+	}
+	newArgs := make(ListK, len(k.args))
+	for i, arg := range k.args {
+		// newArgs = append(newArgs, arg.Copy())
+		newArgs[i] = arg.Copy()
 	}
 	copy := &K{k.label, newArgs, k.value, k.variable}
 	return copy
 }
 
-type KLabel string
-func (kl KLabel) String() string {
-	return string(kl)
-}
+// func (kl KLabel) String() string {
+// 	return string(kl)
+// }
 
 type ListK []*K
 func (lk ListK) String() string {
@@ -53,13 +107,14 @@ func (c Continuation) String() string {
 }
 
 
+
 var stateCell map[KLabel]*K = make(map[KLabel]*K)
 var kCell Continuation = nil
 
 func stateString() string {
 	var state []string = nil
 	for variable, value := range stateCell {
-		state = append(state, string(variable) + " -> " + string(value.label))
+		state = append(state, variable.String() + " -> " + value.label.String())
 	}
 	return "state(" + strings.Join(state, "\n      ") + ")\n" + kCell.String()
 }
@@ -78,6 +133,8 @@ func main() {
 	pprof.StopCPUProfile()
 	delta := time.Since(t0)
 	fmt.Printf("Took %v\n", delta)
+	result := stateCell[label_s]
+	fmt.Printf("Result: %d\n", result)
 }
 
 /*
@@ -90,27 +147,27 @@ while not(n <= 0) do (
 )
 */
 func prog1() *K {
-	n := &K{"n", nil, false, true}
-	s := &K{"s", nil, false, true}
-	l1 := &K{"var", []*K{n, s}, false, false}
-	hundred := &K{"100000", nil, true, false}
-	l2 := &K{":=", []*K{n, hundred}, false, false}
-	zero := &K{"0", nil, true, false}
-	l3 := &K{":=", []*K{s, zero}, false, false}
-	sPn := &K{"+", []*K{s, n}, false, false}
-	l5 := &K{":=", []*K{s, sPn}, false, false}
-	negOne := &K{"-", []*K{&K{"1", nil, true, false}}, false, false}
-	nPno := &K{"+", []*K{n, negOne}, false, false}
-	l6 := &K{":=", []*K{n, nPno}, false, false}
-	body := &K{";", []*K{l5, l6}, false, false}
+	n := &K{label_n, nil, false, true}
+	s := &K{label_s, nil, false, true}
+	l1 := &K{label_var, []*K{n, s}, false, false}
+	hundred := &K{upto, nil, true, false}
+	l2 := &K{label_assign, []*K{n, hundred}, false, false}
+	zero := &K{Int64Label(0), nil, true, false}
+	l3 := &K{label_assign, []*K{s, zero}, false, false}
+	sPn := &K{label_plus, []*K{s, n}, false, false}
+	l5 := &K{label_assign, []*K{s, sPn}, false, false}
+	negOne := &K{label_neg, []*K{&K{Int64Label(1), nil, true, false}}, false, false}
+	nPno := &K{label_plus, []*K{n, negOne}, false, false}
+	l6 := &K{label_assign, []*K{n, nPno}, false, false}
+	body := &K{label_semi, []*K{l5, l6}, false, false}
 
-	nLTzero := &K{"<=", []*K{n, zero}, false, false}
-	guard := &K{"not", []*K{nLTzero}, false, false}
-	l4 := &K{"while", []*K{guard, body}, false, false}
+	nLTzero := &K{label_lte, []*K{n, zero}, false, false}
+	guard := &K{label_not, []*K{nLTzero}, false, false}
+	l4 := &K{label_while, []*K{guard, body}, false, false}
 
-	pgm := &K{";", []*K{l3, l4}, false, false}
-	pgm = &K{";", []*K{l2, pgm}, false, false}
-	pgm = &K{";", []*K{l1, pgm}, false, false}
+	pgm := &K{label_semi, []*K{l3, l4}, false, false}
+	pgm = &K{label_semi, []*K{l2, pgm}, false, false}
+	pgm = &K{label_semi, []*K{l1, pgm}, false, false}
 	return pgm
 }
 
@@ -128,7 +185,7 @@ func repl() {
 			panic("Safety check!")
 		}
 		topSpot := len(kCell) - 1
-		top := kCell[topSpot]		
+		top := kCell[topSpot]
 
 		if top.value == true {
 			if len(kCell) == 1 {
@@ -136,11 +193,11 @@ func repl() {
 			}
 			next := kCell[topSpot - 1]
 			for i, arg := range next.args {
-				if arg.label == "hole" {
+				if arg.label == label_hole {
 					if printDebug { fmt.Printf("Applying 'cooling' rule\n") }
 					change = true
 					kCell = kCell[:topSpot]
-					kCell[topSpot-1] = next.Copy()
+					kCell[topSpot-1] = next
 					kCell[topSpot-1].args[i] = top
 					break
 				}
@@ -152,13 +209,12 @@ func repl() {
 				change = true
 				kCell[topSpot] = value
 			}
-		}
-		if top.label == ";" {
+		} else if top.label == label_semi {
 			if printDebug { fmt.Printf("Applying ';' rule\n") }
 			change = true
 			kCell = append(kCell, top.args[0])
 			kCell[topSpot] = top.args[1]
-		} else if top.label == "var" {
+		} else if top.label == label_var {
 			if len(top.args) == 0 {
 				if printDebug { fmt.Printf("Applying 'var-empty' rule\n") }
 				change = true
@@ -166,19 +222,19 @@ func repl() {
 			} else {
 				if printDebug { fmt.Printf("Applying 'var-something' rule\n") }
 				change = true
-				stateCell[top.args[0].label] = &K{"0", nil, true, false}
-				newTop := top.Copy()
+				stateCell[top.args[0].label] = &K{Int64Label(0), nil, true, false}
+				newTop := top
 				newTop.args = newTop.args[1:]
 				kCell[topSpot] = newTop
 			}
-		} else if top.label == ":=" {
+		} else if top.label == label_assign {
 			right := top.args[1]
 			if !right.value {
 				if printDebug { fmt.Printf("Applying ':=-heat' rule\n") }
 				change = true
 				kCell = append(kCell, right)
-				newTop := top.Copy()
-				newTop.args[1] = &K{"hole", nil, false, false}
+				newTop := top
+				newTop.args[1] = hole
 				kCell[topSpot] = newTop
 			} else {
 				if printDebug { fmt.Printf("Applying 'assign' rule\n") }
@@ -187,129 +243,124 @@ func repl() {
 				stateCell[variable] = right
 				kCell = kCell[:topSpot]
 			}
-		} else if top.label == "while" {
+		} else if top.label == label_while {
 			if printDebug { fmt.Printf("Applying 'while' rule\n") }
 			change = true
-			skip := &K{"skip", nil, false, false}
+			skip := &K{label_skip, nil, false, false}
 			guard := top.args[0]
 			body := top.args[1]
-			then := &K{";", []*K{body, top}, false, false}
-			theIf := &K{"if", []*K{guard, then, skip}, false, false}
+			then := &K{label_semi, []*K{body, top.Copy()}, false, false}
+			theIf := &K{label_if, []*K{guard, then, skip}, false, false}
 			kCell[topSpot] = theIf
-		} else if top.label == "if" {
+		} else if top.label == label_if {
 			guard := top.args[0]
 			if !guard.value {
 				if printDebug { fmt.Printf("Applying 'if-heat' rule\n") }
 				change = true
 				kCell = append(kCell, guard)
-				newTop := top.Copy()
-				newTop.args[0] = &K{"hole", nil, false, false}
+				newTop := top
+				newTop.args[0] = hole
 				kCell[topSpot] = newTop
 			} else {
-				if guard.label == "true" {
+				if guard.label == label_true {
 					if printDebug { fmt.Printf("Applying 'if-true' rule\n") }
 					change = true
 					kCell[topSpot] = top.args[1]
-				} else if guard.label == "false" {
+				} else if guard.label == label_false {
 					if printDebug { fmt.Printf("Applying 'if-false' rule\n") }
 					change = true
 					kCell[topSpot] = top.args[2]
 				}
 			}
-		} else if top.label == "not" {
+		} else if top.label == label_not {
 			body := top.args[0]
 			if !body.value {
 				if printDebug { fmt.Printf("Applying 'not-heat' rule\n") }
 				change = true
 				kCell = append(kCell, body)
-				newTop := top.Copy()
-				newTop.args[0] = &K{"hole", nil, false, false}
+				newTop := top
+				newTop.args[0] = hole
 				kCell[topSpot] = newTop
 			} else {
-				if top.args[0].label == "false" {
+				if top.args[0].label == label_false {
 					if printDebug { fmt.Printf("Applying 'not-false' rule\n") }
 					change = true
-					kCell[topSpot] = &K{"true", nil, true, false}
-				} else if top.args[0].label == "true" {
+					kCell[topSpot] = &K{label_true, nil, true, false}
+				} else if top.args[0].label == label_true {
 					if printDebug { fmt.Printf("Applying 'not-true' rule\n") }
 					change = true
-					kCell[topSpot] = &K{"false", nil, true, false}
+					kCell[topSpot] = &K{label_false, nil, true, false}
 				}
 			}
-		} else if top.label == "<=" {
+		} else if top.label == label_lte {
 			left := top.args[0]
 			right := top.args[1]
 			if !left.value {
 				if printDebug { fmt.Printf("Applying '<=-heat-left' rule\n") }
 				change = true
 				kCell = append(kCell, left)
-				newTop := top.Copy()
-				newTop.args[0] = &K{"hole", nil, false, false}
+				newTop := top
+				newTop.args[0] = hole
 				kCell[topSpot] = newTop
 			} else if !right.value {
 				if printDebug { fmt.Printf("Applying '<=-heat-right' rule\n") }
 				change = true
 				kCell = append(kCell, right)
-				newTop := top.Copy()
-				newTop.args[1] = &K{"hole", nil, false, false}
+				newTop := top
+				newTop.args[1] = hole
 				kCell[topSpot] = newTop
 			} else {
 				if printDebug { fmt.Printf("Applying '<=' rule\n") }
 				change = true
-				leftv, err := strconv.ParseInt(string(left.label), 10, 64)
-				if err != nil { panic(err) }
-				rightv, err := strconv.ParseInt(string(right.label), 10, 64)
-				if err != nil { panic(err) }
+				leftv := left.label.(Int64Label)
+				rightv := right.label.(Int64Label)
 				if leftv <= rightv {
-					kCell[topSpot] = &K{"true", nil, true, false}
+					kCell[topSpot] = &K{label_true, nil, true, false}
 				} else {
-					kCell[topSpot] = &K{"false", nil, true, false}
+					kCell[topSpot] = &K{label_false, nil, true, false}
 				}
 			}
-		} else if top.label == "+" {
+		} else if top.label == label_plus {
 			left := top.args[0]
 			right := top.args[1]
 			if !left.value {
 				if printDebug { fmt.Printf("Applying '+-heat-left' rule\n") }
 				change = true
 				kCell = append(kCell, left)
-				newTop := top.Copy()
-				newTop.args[0] = &K{"hole", nil, false, false}
+				newTop := top
+				newTop.args[0] = hole
 				kCell[topSpot] = newTop
 			} else if !right.value {
 				if printDebug { fmt.Printf("Applying '+-heat-right' rule\n") }
 				change = true
 				kCell = append(kCell, right)
-				newTop := top.Copy()
-				newTop.args[1] = &K{"hole", nil, false, false}
+				newTop := top
+				newTop.args[1] = hole
 				kCell[topSpot] = newTop
 			} else {
 				if printDebug { fmt.Printf("Applying '+' rule\n") }
 				change = true
-				leftv, err := strconv.ParseInt(string(left.label), 10, 64)
-				if err != nil { panic(err) }
-				rightv, err := strconv.ParseInt(string(right.label), 10, 64)
-				if err != nil { panic(err) }
-				kCell[topSpot] = &K{KLabel(fmt.Sprintf("%d", leftv + rightv)), nil, true, false}
+				leftv := left.label.(Int64Label)
+				rightv := right.label.(Int64Label)
+				kCell[topSpot] = &K{Int64Label(leftv + rightv), nil, true, false}
 			}
-		} else if top.label == "-" {
+		} else if top.label == label_neg {
 			body := top.args[0]
 			if !body.value {
 				if printDebug { fmt.Printf("Applying 'neg-heat' rule\n") }
 				change = true
 				kCell = append(kCell, body)
-				newTop := top.Copy()
-				newTop.args[0] = &K{"hole", nil, false, false}
+				newTop := top
+				newTop.args[0] = hole
 				kCell[topSpot] = newTop
 			} else {
 				if printDebug { fmt.Printf("Applying 'minus' rule\n") }
 				change = true
-				value, err := strconv.ParseInt(string(body.label), 10, 64)
-				if err != nil { panic(err) }
-				newValue := fmt.Sprintf("%d", -value)
-				kCell[topSpot] = &K{KLabel(newValue), nil, true, false}
+				value := body.label.(Int64Label)
+				newValue := -value
+				kCell[topSpot] = &K{Int64Label(newValue), nil, true, false}
 			}
-		} else if top.label == "skip" {
+		} else if top.label == label_skip {
 			if printDebug { fmt.Printf("Applying 'skip' rule\n") }
 			change = true
 			kCell = kCell[:topSpot]
