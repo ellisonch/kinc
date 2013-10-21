a4a1fbe56e2e363adf4d1ec858e8cff89be7eff6
Chucky Ellison <cme@freefour.com>
2013-10-20 20:16:48 -0400

    have imp working with collatz

--- imp.go
+++ imp.go
@@ -6,43 +6,48 @@ import "time"
 import "runtime/pprof"
 import "os"
 import . "github.com/ellisonch/k"
+import "github.com/ellisonch/aterm"
 
 // need to deal with terms inside the store
 
 const profile = true
 const shouldCheck = false
-const upto = 5000000
 const printDebug = false
+// const upto = 1
 
-var label_semi KLabel 	= StringLabel("Semi")	
+
+var label_statements KLabel = StringLabel("Statements")	
 var label_var KLabel 	= StringLabel("Var")
 var label_skip KLabel 	= StringLabel("Skip")
 var label_plus KLabel 	= StringLabel("Plus")
+var label_div KLabel 	= StringLabel("Div")
 var label_neg KLabel 	= StringLabel("Neg")
 var label_assign KLabel = StringLabel("Assign")
 var label_while KLabel 	= StringLabel("While")
 var label_if KLabel 	= StringLabel("If")
 var label_not KLabel 	= StringLabel("Not")
 var label_lte KLabel 	= StringLabel("LTE")
-var label_n KLabel 		= StringLabel("n")
-var label_s KLabel 		= StringLabel("s")
 var label_true KLabel 	= StringLabel("True")
 var label_false KLabel 	= StringLabel("False")
+var label_id KLabel		= StringLabel("Id")
+var label_bool KLabel	= StringLabel("Bool")
+var label_program KLabel= StringLabel("Program")
+var label_paren KLabel	= StringLabel("Paren")
 var label_zero KLabel 	= Int64Label(0)
-var label_one KLabel 	= Int64Label(1)
 
-func k_true() *K { return NewKSpecial(label_true, nil, true, false) }
-func k_false() *K { return NewKSpecial(label_false, nil, true, false) }
-func k_zero() *K { return NewKSpecial(label_zero, nil, true, false) }
-func k_one() *K { return NewKSpecial(label_one, nil, true, false) }
+func k_true() *K { return NewK(label_bool, []*K{NewK(label_true, nil)}) }
+func k_false() *K { return NewK(label_bool, []*K{NewK(label_false, nil)}) }
+func k_zero() *K { return NewK(Label_int, []*K{NewK(label_zero, nil)}) }
 func k_skip() *K { return NewK(label_skip, nil) }
-// func label_upto() *K { return Int64Label(upto) }
 
-// var k_true = NewKSpecial(label_true, nil, true, false)
-// var k_false = NewKSpecial(label_false, nil, true, false)
-// var k_zero = NewKSpecial(label_zero, nil, true, false)
-// var k_one = NewKSpecial(label_one, nil, true, false)
-var label_upto KLabel 	= Int64Label(upto)
+
+func isValue(k *K) bool {
+	if k.P_Label() == Label_int || k.P_Label() == label_bool {
+		return true
+	} else {
+		return false
+	}
+}
 // var k_skip = NewK(label_skip, nil)
 
 
@@ -78,18 +83,33 @@ var kCell Continuation = nil
 func stateString() string {
 	var state []string = nil
 	for variable, value := range stateCell {
-		state = append(state, variable + " -> " + value.P_Label().String())
+		state = append(state, variable + " -> " + value.String())
 	}
 	return "state(" + strings.Join(state, "\n      ") + ")\n" + kCell.String()
+}
+
+func getProgram() *K {
+	l := aterm.NewATermLexer(os.Stdin)
+	ret := aterm.ATermParse(l)
+	if ret != 0 { 
+		panic("Couldn't parse ATerm!")
+		// fmt.Printf("%s\n\n", aterm.FinalTerm.String())		
+	}
+	newk := ATermToK(&aterm.FinalTerm)
+	// if newk != nil {
+	// 	fmt.Printf("%s\n", newk.String())
+	// } else {
+	// 	fmt.Printf("nil")
+	// }
+	return newk
 }
 
 func main() {
 	f, err := os.Create("profiling.dat")
     if err != nil { panic(err) }
-
-	Init()
-
-	appendK(prog1())
+    p1 := getProgram()
+    // fmt.Printf("%s\n", p1.String())
+	appendK(p1)
 	t0 := time.Now()
 
 	if profile {
@@ -101,47 +121,12 @@ func main() {
 	}
 	delta := time.Since(t0)
 	fmt.Printf("Took %v\n", delta)
-	result := stateCell[label_s.P_Str()].P_Label().P_Data()
-	fmt.Printf("Result: %d\n", result)
-}
-
-/*
-var n, s ;
-n := 100 ;
-s := 0 ;
-while not(n <= 0) do (
-	s := s + n ;
-	n := n + -1
-)
-*/
-func prog1() *K {
-	n := NewKSpecial(label_n, nil, false, true)
-	s := NewKSpecial(label_s, nil, false, true)
-	hundred := NewKSpecial(label_upto, nil, true, false)
-
-	l1 := NewK(label_var, []*K{n, s})
+	if resultk, ok := stateCell["s"]; ok {
+		result := resultk.Inner().P_Label().P_Data()
+		fmt.Printf("Result: %d\n", result)
+	}
 	
-	l2 := NewK(label_assign, []*K{n, hundred})
-	
-	l3 := NewK(label_assign, []*K{s, k_zero()})
-	sPn := NewK(label_plus, []*K{s, n})
-	l5 := NewK(label_assign, []*K{s, sPn})
-	negOne := NewK(label_neg, []*K{k_one()})
-	nPno := NewK(label_plus, []*K{n, negOne})
-	l6 := NewK(label_assign, []*K{n, nPno})
-	body := NewK(label_semi, []*K{l5, l6})
-
-	nLTzero := NewK(label_lte, []*K{n, k_zero()})
-	guard := NewK(label_not, []*K{nLTzero})
-	l4 := NewK(label_while, []*K{guard, body})
-
-	pgm := NewK(label_semi, []*K{l3, l4})
-	pgm = NewK(label_semi, []*K{l2, pgm})
-	pgm = NewK(label_semi, []*K{l1, pgm})
-	return pgm
 }
-
-
 
 func trimK() {
 	top := len(kCell)-1
@@ -190,11 +175,12 @@ func handleValue() bool {
 }
 
 func handleVariable() bool {
+	// fmt.Printf("Handling variable")
 	change := false
 	topSpot := len(kCell) - 1
 	top := kCell[topSpot]
 
-	variable := top.P_Label()
+	variable := top.Inner().P_Label()
 	if value, ok := stateCell[variable.P_Str()]; ok {
 		if printDebug { fmt.Printf("Applying 'lookup' rule\n") }
 		change = true
@@ -203,17 +189,28 @@ func handleVariable() bool {
 	return change
 }
 
-func handleSemi() bool {
+func handleStatements() bool {
 	change := false
 	topSpot := len(kCell) - 1
 	top := kCell[topSpot]
 
-	if printDebug { fmt.Printf("Applying ';' rule\n") }
-	change = true
-	left := top.P_Args()[0]
-	right := top.P_Args()[1]
-	appendK(left)
-	setPreHead(right)
+	if len(top.P_Args()) == 0 {
+		if printDebug { fmt.Printf("Applying 'statements-empty' rule\n") }
+		change = true
+		trimK()
+	} else if len(top.P_Args()) == 1 {
+		if printDebug { fmt.Printf("Applying 'statements-one' rule\n") }
+		change = true
+		newTop := top.P_Args()[0]		
+		setHead(newTop)
+	} else {
+		if printDebug { fmt.Printf("Applying 'statements-many' rule\n") }
+		change = true
+		appendK(top.P_Args()[0])
+		newPreHead := UpdateTrimArgs(top, 1, len(top.P_Args()))
+		setPreHead(newPreHead)
+	}
+
 	return change
 }
 
@@ -239,7 +236,7 @@ func handleVar() bool {
 	} else {
 		if printDebug { fmt.Printf("Applying 'var-something' rule\n") }
 		change = true
-		updateStore(top.P_Args()[0], k_zero())
+		updateStore(top.Inner().Inner(), k_zero())
 		newTop := UpdateTrimArgs(top, 1, len(top.P_Args()))
 		setHead(newTop)
 	}
@@ -252,7 +249,7 @@ func handleAssign() bool {
 	top := kCell[topSpot]
 
 	right := top.P_Args()[1]
-	if !right.P_Value() {
+	if !isValue(right) {
 		if printDebug { fmt.Printf("Applying ':=-heat' rule\n") }
 		change = true
 		appendK(right)
@@ -263,7 +260,7 @@ func handleAssign() bool {
 	} else {
 		if printDebug { fmt.Printf("Applying 'assign' rule\n") }
 		change = true
-		updateStore(top.P_Args()[0], right)
+		updateStore(top.Inner().P_Args()[0], right)
 		trimK()
 	}
 	return change
@@ -278,7 +275,7 @@ func handleWhile() bool {
 	change = true
 	guard := top.P_Args()[0]
 	body := top.P_Args()[1]
-	then := NewK(label_semi, []*K{body, top})
+	then := NewK(label_statements, []*K{body, top})
 	theIf := NewK(label_if, []*K{guard, then, k_skip()})
 	setHead(theIf)
 
@@ -291,18 +288,18 @@ func handleIf() bool {
 	top := kCell[topSpot]
 
 	guard := top.P_Args()[0]
-	if !guard.P_Value() {
+	if !isValue(guard) {
 		if printDebug { fmt.Printf("Applying 'if-heat' rule\n") }
 		change = true
 		appendK(guard)
 		newTop := UpdateArg(top, 0, Hole)
 		setPreHead(newTop)
 	} else {
-		if guard.P_Label() == label_true {
+		if guard.Inner().P_Label() == label_true {
 			if printDebug { fmt.Printf("Applying 'if-true' rule\n") }
 			change = true
 			setHead(top.P_Args()[1])
-		} else if guard.P_Label() == label_false {
+		} else if guard.Inner().P_Label() == label_false {
 			if printDebug { fmt.Printf("Applying 'if-false' rule\n") }
 			change = true
 			setHead(top.P_Args()[2])
@@ -317,19 +314,19 @@ func handleNot() bool {
 	topSpot := len(kCell) - 1
 	top := kCell[topSpot]
 
-	body := top.P_Args()[0]
-	if !body.P_Value() {
+	body := top.Inner()
+	if !isValue(body) {
 		if printDebug { fmt.Printf("Applying 'not-heat' rule\n") }
 		change = true
 		appendK(body)
 		newTop := UpdateArg(top, 0, Hole)
 		setPreHead(newTop)
 	} else {
-		if top.P_Args()[0].P_Label() == label_false {
+		if top.Inner().Inner().P_Label() == label_false {
 			if printDebug { fmt.Printf("Applying 'not-false' rule\n") }
 			change = true
 			setHead(k_true())
-		} else if top.P_Args()[0].P_Label() == label_true {
+		} else if top.Inner().Inner().P_Label() == label_true {
 			if printDebug { fmt.Printf("Applying 'not-true' rule\n") }
 			change = true
 			setHead(k_false())
@@ -346,13 +343,13 @@ func handleLTE() bool {
 
 	left := top.P_Args()[0]
 	right := top.P_Args()[1]
-	if !left.P_Value() {
+	if !isValue(left) {
 		if printDebug { fmt.Printf("Applying '<=-heat-left' rule\n") }
 		change = true
 		appendK(left)
 		newTop := UpdateArg(top, 0, Hole)
 		setPreHead(newTop)
-	} else if !right.P_Value() {
+	} else if !isValue(right) {
 		if printDebug { fmt.Printf("Applying '<=-heat-right' rule\n") }
 		change = true
 		appendK(right)
@@ -361,8 +358,8 @@ func handleLTE() bool {
 	} else {
 		if printDebug { fmt.Printf("Applying '<=' rule\n") }
 		change = true
-		leftv := left.P_Label().P_Data()
-		rightv := right.P_Label().P_Data()
+		leftv := left.Inner().P_Label().P_Data()
+		rightv := right.Inner().P_Label().P_Data()
 		if leftv <= rightv {
 			setHead(k_true())
 		} else {
@@ -380,13 +377,13 @@ func handlePlus() bool {
 
 	left := top.P_Args()[0]
 	right := top.P_Args()[1]
-	if !left.P_Value() {
+	if !isValue(left) {
 		if printDebug { fmt.Printf("Applying '+-heat-left' rule\n") }
 		change = true
 		appendK(left)
 		newTop := UpdateArg(top, 0, Hole)
 		setPreHead(newTop)
-	} else if !right.P_Value() {
+	} else if !isValue(right) {
 		if printDebug { fmt.Printf("Applying '+-heat-right' rule\n") }
 		change = true
 		appendK(right)
@@ -395,9 +392,40 @@ func handlePlus() bool {
 	} else {
 		if printDebug { fmt.Printf("Applying '+' rule\n") }
 		change = true
-		leftv := left.P_Label().P_Data()
-		rightv := right.P_Label().P_Data()
-		newTop := NewKSpecial(Int64Label(leftv + rightv), nil, true, false)
+		leftv := left.Inner().P_Label().P_Data()
+		rightv := right.Inner().P_Label().P_Data()
+		newTop := NewK(Label_int, []*K{NewK(Int64Label(leftv + rightv), nil)})
+		setHead(newTop)
+	}
+
+	return change
+}
+
+func handleDiv() bool {
+	change := false
+	topSpot := len(kCell) - 1
+	top := kCell[topSpot]
+
+	left := top.P_Args()[0]
+	right := top.P_Args()[1]
+	if !isValue(left) {
+		if printDebug { fmt.Printf("Applying '/-heat-left' rule\n") }
+		change = true
+		appendK(left)
+		newTop := UpdateArg(top, 0, Hole)
+		setPreHead(newTop)
+	} else if !isValue(right) {
+		if printDebug { fmt.Printf("Applying '/-heat-right' rule\n") }
+		change = true
+		appendK(right)
+		newTop := UpdateArg(top, 1, Hole)
+		setPreHead(newTop)
+	} else {
+		if printDebug { fmt.Printf("Applying '/' rule\n") }
+		change = true
+		leftv := left.Inner().P_Label().P_Data()
+		rightv := right.Inner().P_Label().P_Data()
+		newTop := NewK(Label_int, []*K{NewK(Int64Label(leftv / rightv), nil)})
 		setHead(newTop)
 	}
 
@@ -409,8 +437,8 @@ func handleNeg() bool {
 	topSpot := len(kCell) - 1
 	top := kCell[topSpot]
 
-	body := top.P_Args()[0]
-	if !body.P_Value() {
+	body := top.Inner()
+	if !isValue(body) {
 		if printDebug { fmt.Printf("Applying 'neg-heat' rule\n") }
 		change = true
 		appendK(body)
@@ -419,9 +447,9 @@ func handleNeg() bool {
 	} else {
 		if printDebug { fmt.Printf("Applying 'neg' rule\n") }
 		change = true
-		value := body.P_Label().P_Data()
+		value := body.Inner().P_Label().P_Data()
 		newValue := -value
-		newTop := NewKSpecial(Int64Label(newValue), nil, true, false)
+		newTop := NewK(Label_int, []*K{NewK(Int64Label(newValue), nil)})
 		setHead(newTop)
 	}
 
@@ -434,6 +462,30 @@ func handleSkip() bool {
 	if printDebug { fmt.Printf("Applying 'skip' rule\n") }
 	change = true
 	trimK()
+
+	return change
+}
+
+func handleProgram() bool {
+	change := false
+	topSpot := len(kCell) - 1
+	top := kCell[topSpot]
+
+	if printDebug { fmt.Printf("Applying 'program' rule\n") }
+	change = true
+	setHead(top.Inner())
+
+	return change
+}
+
+func handleParen() bool {
+	change := false
+	topSpot := len(kCell) - 1
+	top := kCell[topSpot]
+
+	if printDebug { fmt.Printf("Applying 'paren' rule\n") }
+	change = true
+	setHead(top.Inner())
 
 	return change
 }
@@ -456,24 +508,24 @@ func repl() {
 		topSpot := len(kCell) - 1
 		top := kCell[topSpot]
 
-		if top.P_Value() == true {
-			change = handleValue()
-		} else if top.P_Variable() == true {
-			change = handleVariable()
-		} else {
-			var topLabel KLabel = top.P_Label()
-			switch {
-				case topLabel.Equals(label_semi): change = handleSemi()
-				case topLabel.Equals(label_var): change = handleVar()
-				case topLabel.Equals(label_assign): change = handleAssign()
-				case topLabel.Equals(label_while): change = handleWhile()
-				case topLabel.Equals(label_if): change = handleIf()
-				case topLabel.Equals(label_not): change = handleNot()
-				case topLabel.Equals(label_lte): change = handleLTE()
-				case topLabel.Equals(label_plus): change = handlePlus()
-				case topLabel.Equals(label_neg): change = handleNeg()
-				case topLabel.Equals(label_skip): change = handleSkip()
-			}
+		var topLabel KLabel = top.P_Label()
+		switch {
+			case isValue(top): change = handleValue()
+			case topLabel.Equals(label_id): change = handleVariable()
+			case topLabel.Equals(label_statements): change = handleStatements()
+			case topLabel.Equals(label_var): change = handleVar()
+			case topLabel.Equals(label_assign): change = handleAssign()
+			case topLabel.Equals(label_while): change = handleWhile()
+			case topLabel.Equals(label_if): change = handleIf()
+			case topLabel.Equals(label_not): change = handleNot()
+			case topLabel.Equals(label_lte): change = handleLTE()
+			case topLabel.Equals(label_plus): change = handlePlus()
+			case topLabel.Equals(label_div): change = handleDiv()
+			case topLabel.Equals(label_neg): change = handleNeg()
+			case topLabel.Equals(label_program): change = handleProgram()
+			case topLabel.Equals(label_paren): change = handleParen()
+			// case topLabel.Equals(label_neg): change = handleNeg()
+			case topLabel.Equals(label_skip): change = handleSkip()
 		}
 	}
 }
