582db425b129ae7a764585816b33cef6564010e7
Chucky Ellison <cme@freefour.com>
2015-02-14 19:55:15 -0500

    added some needed newlines

--- imp/settings.h
+++ imp/settings.h
@@ -9,7 +9,8 @@
 
 #define printDebug 0
 
-#define shouldCheck 1
+// slow checks dealing with refcounting
+#define shouldCheck 0
 
 // technically not needed, but good to be safe
 #define checkTypeSafety 1
