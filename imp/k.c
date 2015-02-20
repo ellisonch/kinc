51d040707848c8cca3868e8a618a24abd4c7250d
Chucky Ellison <cme@freefour.com>
2015-02-19 22:22:34 -0500

    cleaned up and made sure to clean up args in case k buffer spills

--- imp/k.c
+++ imp/k.c
@@ -28,46 +28,6 @@ int count_malloc_listk_array;
 
 int count_malloc_k = 0;
 
-
-// int list_get_right_size_offset(int n) {
-// 	if (n <= 16) {
-// 		return 0;
-// 	}
-// 	return ceil_log2(n);
-// }
-
-// int list_get_right_size(int n) {
-// 	if (n == 0) {
-// 		return 1;
-// 	}
-// 	// if (n <= 16) {
-// 	// 	return 16;
-// 	// }
-// 	return next_highest_power(n);
-// }
-
-// ListK* _getDeadList(int reqLength) {
-// 	if (reqLength > MAX_GARBAGE_ARG_LEN) {
-// 		return NULL;
-// 	}
-// 	if (garbage_listk_next == 0) {
-// 		if (printDebug) { printf("No dead stuff of length %d\n", reqLength); }
-// 		return NULL;
-// 	}
-
-// 	ListK* ret = garbage_listk[garbage_listk_next - 1];
-// 	assert(ret != NULL);
-// 	assert(ret->cap >= reqLength);
-
-// 	garbage_listk_next--;
-// 	// ret->len = reqLength;
-
-// 	if (printDebug) {
-// 		printf("Returning dead list of length %d\n", reqLength);
-// 	}
-// 	return ret;
-// }
-
 ListK* _mallocArgs() {
 	count_malloc_listk++;
 	return malloc(sizeof(ListK));
@@ -240,7 +200,7 @@ char* KToString(K* k) {
 	return s;
 }
 
-void terminate_args(ListK* args) {
+void free_args(ListK* args) {
 	assert(args != NULL);
 	assert(args->a != NULL);
 	assert(count_malloc_listk >= 1);
@@ -255,29 +215,6 @@ void terminate_args(ListK* args) {
 	free(args);
 }
 
-// int COMPLAINED_MAX_GARBAGE_ARG_LEN = 0;
-// // we're done with a ListK, and we need to see if we can reuse it
-// void dispose_args(K* k) {	
-// 	assert(k != NULL);
-// 	assert(k->args != NULL);
-
-// 	ListK* args = k->args;
-// 	k->args = NULL;
-
-// 	// if we already have too many args of this many arguments, then kill it
-// 	assert(garbage_listk_next <= MAX_GARBAGE_KEPT);
-// 	if (garbage_listk_next == MAX_GARBAGE_KEPT) {
-// 		terminate_args(args);
-// 		return;
-// 	}
-
-// 	// save it
-// 	garbage_listk[garbage_listk_next] = args;	
-// 	garbage_listk_next++;
-
-// 	if (printDebug) { printf("Saving args\n"); }
-// }
-
 void dispose_k_itself(K* k) {
 	assert(k != NULL);
 
@@ -286,6 +223,8 @@ void dispose_k_itself(K* k) {
 		garbage_k[garbage_k_next++] = k;
 	} else {
 		if (printDebug) { printf("Freeing k\n"); }
+		free_args(k->args);
+		k->args = NULL;
 		count_malloc_k--;
 		free(k);
 	}
@@ -349,8 +288,6 @@ void dispose_k(K* k) {
 	// }
 	
 	dispose_k_aux(k);
-	
-	// fflush(stdout);
 }
 
 void Dec(K* k) {
@@ -359,21 +296,7 @@ void Dec(K* k) {
 
 	k->refs--;
 	int newRefs = k->refs;
-	// if (checkRefCounting) {
-	// 	if (newRefs < 0) {
-	// 		panic("Term %s has fewer than 0 refs :(", KToString(k));
-	// 	}
-	// }
 	if (newRefs == 0) {
-		// printf("Dead term found: %s", KToString(k));
-		// ListK* args = k->args;
-
-		// // since k is dead, we can remove one ref from any of its children
-		// for (int i = 0; i < args->len; i++) {
-		// 	K* arg = args->a[i];
-		// 	Dec(arg);
-		// }
-
 		dispose_k(k);
 	}
 }
@@ -390,7 +313,6 @@ K* copy(K* oldK) {
 	}
 	return k;
 }
-
 
 // updates an arg from a k to another k
 // sometimes a copy needs to be made, and this function does not Dec() the old k, so make sure you do
@@ -418,14 +340,6 @@ K* k_set_arg(K* orig_k, int arg, K* newVal) {
 		free(sk);
 	}
 	Dec(orig_arg);
-	// if (k != orig_k) {
-		
-	// 	printf("Was %s\n", KToString(orig_k));
-	// 	printf("Now %s\n\n", KToString(k));
-	// 	// Dec(orig_k);
-	// } else {
-	// 	// Dec(orig_arg);
-	// }
 
 	return k;
 }
@@ -500,37 +414,7 @@ void dump_garbage_info() {
 	printf("\ncount_malloc_k: %d\n", count_malloc_k);
 	printf("garbage_k_next: %d\n\n", garbage_k_next);
 
-	// for (int i = 0; i < MAX_GARBAGE_ARG_LEN; i++) {
-	// 	for (int j = 0; j < garbage_listk_nexts[i]; j++) {
-	// 		ListK* args = garbage_listk[i][j];
-	// 		if (args->len > 0) {
-	// 			if (args->cap < MAX_MALLOC_LISTK_ARRAY_SIZE) {
-	// 				count_malloc_listk_array[args->cap]--;
-	// 			}
-	// 			// free(args->a);
-	// 		}
-	// 		count_malloc_listk--;
-	// 		// free(args);
-	// 	}
-	// }
-	// deadListsLen[0] = 0;
-	// deadListsLen[1] = 0;
-	// deadListsLen[2] = 0;
-	// deadListsLen[3] = 0; // for some reason this is causing a segfault with clang
-	// deadListsLen[4] = 0;
-
-	// for (int i = 0; i < MAX_GARBAGE_ARG_LEN-1; i++) {
-	// 	deadListsLen[i] = 0;
-	// 	// printf("%d\n", deadListsLen[i]);
-	// }
 	printf("count_malloc_listk_array: %d\n", count_malloc_listk_array);
-	// for (int i = 0; i < MAX_MALLOC_LISTK_ARRAY_SIZE; i++) {
-	// 	printf("count_malloc_listk_array %d: %d\n", i, count_malloc_listk_array[i]);
-	// }
-	// for (int i = 0; i < MAX_GARBAGE_ARG_LEN; i++) {
-	// 	printf("garbage_listk_nexts %d: %d\n", i, garbage_listk_nexts[i]);
-	// }
-	// printf("garbage_listk_next: %d\n", garbage_listk_next);
 	printf("count_malloc_listk: %d\n\n", count_malloc_listk);
 
 	dump_label_garbage_info();
@@ -621,13 +505,8 @@ void k_init() {
 	k_init_builtins();
 
 	// preallocate garbage; doesn't seem to help
-	// for (int i = 0; i < MAX_GARBAGE_KEPT; i++) {
-	// 	garbage_k[i] = mallocK();
+	// for (int i = 0; i < 100; i++) {
+	// 	garbage_k[i] = _k_acquire(0, 0);
 	// 	garbage_k_next++;
-
-	// 	garbage_listk[i] = listk_create(0, 0);
-	// 	garbage_listk_next++;
 	// }
-	// assert(garbage_k_next == MAX_GARBAGE_KEPT);
-	// assert(garbage_listk_next == MAX_GARBAGE_KEPT);
 }
