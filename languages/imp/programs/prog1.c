267245619f26692174b8d6f061af7f47a02132bb
Chucky Ellison <cme@freefour.com>
2015-03-23 11:39:01 -0400

    playing with benchmarking

--- languages/imp/programs/prog1.c
+++ languages/imp/programs/prog1.c
@@ -2,7 +2,6 @@
 #include <stdint.h>
 #include <inttypes.h>
 
-// 50000000
 int main(int argc, char* argv[]) {
 	uint64_t n = atoll(argv[1]);
 	uint64_t s = 0;
--- languages/imp/programs/prog2.c
+++ languages/imp/programs/prog2.c
@@ -3,11 +3,11 @@
 #include <inttypes.h>
 
 int main(void) {
-	uint64_t m = 7000;
-	uint64_t n = 0;
-	uint64_t q = 0;
-	uint64_t r = 0;
-	uint64_t s = 0;
+	int m = 500000;
+	int n = 0;
+	int q = 0;
+	int r = 0;
+	int s = 0;
 
 	while (!(m <= 2)) {
 		n = m ;
